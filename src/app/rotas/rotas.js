8b3c295d70e517904130364899512b658b2ef839
Rafael Borges <rafaelbertelli89@gmail.com>
2019-10-12 15:12:24 -0300

    Add method to save form

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,25 +1,44 @@
 const db = require("../../config/database");
-const LivroDao = require("../infra/livro-dao")
+const LivroDao = require("../infra/livro-dao");
+
+const _getLivros = res => {
+  const livroDao = new LivroDao(db);
+
+  return livroDao
+    .lista()
+    .then(livros =>
+      res.marko(require("../views/livros/lista/lista.marko"), {
+        livros
+      })
+    )
+    .catch(erro => erro);
+};
 
 module.exports = app => {
   app.get("/", function(req, res) {
     res.marko(require("../views/home/home.marko"));
   });
 
-  app.get("/livros", function(req, res) {
+  app.get("/livros", async function(req, res) {
+    try {
+      await _getLivros(res);
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  app.post("/livros", async function(req, res) {
     const livroDao = new LivroDao(db);
 
-    livroDao
-      .lista()
-      .then(livros => 
-        res
-          .marko(
-            require("../views/livros/lista/lista.marko"),
-            {
-              livros
-            }
-          )
-      )
-      .catch(erro => console.log(erro))
+    try {
+      await livroDao.adiciona(req.body);
+      res.redirect("/livros");
+    } catch (erro) {
+      erro => console.log(erro);
+    }
+  });
+
+  app.get("/livros/form", function(req, res) {
+    res.marko(require("../views/livros/form/form.marko"));
   });
 };
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -3,6 +3,13 @@ require("marko/express");
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
 const rotas = require("../app/rotas/rotas");
 rotas(app);
